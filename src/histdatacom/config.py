00e31162adef4ba5d477248fd9b2e745d098481d
David Midlo <dmidlo@gmail.com>
2022-12-02 15:28:09 -0600

    cleaned histdatacom/config.py

--- src/histdatacom/config.py
+++ src/histdatacom/config.py
@@ -1,5 +1,12 @@
-from multiprocessing.managers import SyncManager
-from histdatacom.records import Records
+"""Configure global attributes."""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from multiprocessing.managers import SyncManager
+
+    from histdatacom.records import Records
 
 ARGS: dict = {}
 REPO_DATA: dict = {}
@@ -15,22 +22,22 @@ INFLUX_CHUNKS_QUEUE: None = None
 REQUESTS_TIMEOUT: int = 10
 
 POST_HEADERS: dict = {
-            "Host": "www.histdata.com",
-            "Connection": "keep-alive",
-            "Content-Length": "101",
-            "Cache-Control": "max-age=0",
-            "Origin": "http://www.histdata.com",
-            "Upgrade-Insecure-Requests": "1",
-            "DNT": "1",
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Accept": (
-                "text/html,application/xhtml+xml,application/xml;"
-                "q=0.9,image/avif,image/webp,image/apng,*/*;"
-                "q=0.8,application/signed-exchange;"
-                "v=b3;"
-                "q=0.9"
-            ),
-            "Referer": "",
-            "Accept-Encoding": "gzip, deflate",
-            "Accept-Language": "en-US,en;q=0.9",
-        }
+    "Host": "www.histdata.com",
+    "Connection": "keep-alive",
+    "Content-Length": "101",
+    "Cache-Control": "max-age=0",
+    "Origin": "http://www.histdata.com",
+    "Upgrade-Insecure-Requests": "1",
+    "DNT": "1",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Accept": (
+        "text/html,application/xhtml+xml,application/xml;"
+        "q=0.9,image/avif,image/webp,image/apng,*/*;"
+        "q=0.8,application/signed-exchange;"
+        "v=b3;"
+        "q=0.9"
+    ),
+    "Referer": "",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "en-US,en;q=0.9",
+}
