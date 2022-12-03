3acf237efe9f8175ffdf94567733bfde2c2c0749
David Midlo <dmidlo@gmail.com>
2022-12-02 19:35:21 -0600

    cleaned histdatacom/histdata_com.py

--- src/histdatacom/histdata_com.py
+++ src/histdatacom/histdata_com.py
@@ -1,41 +1,67 @@
-from pyarrow import Table
-from datatable import Frame
-from pandas import DataFrame
+"""Run main application. Core logic.
 
-from histdatacom import config
-from histdatacom import Options
-from histdatacom.concurrency import QueueManager
-from histdatacom.cli import ArgParser
-from histdatacom.csvs import Csv
+Raises:
+    SystemExit: Exit when complete.
+
+Returns:
+    QueueManager: multi-process serial communication manager
+    repo_data (set): a set of repo pairs with start and end date ranges.
+    Data (Frame | DataFrame | Table):
+        a datatable Frame, a pandas DataFrame, or a pyarrow Table
+    List Of Data:   [
+                        {
+                            "timeframe": timeframe,
+                            "pair": pair,
+                            "records": [record, record, ...],
+                            "data": Frame | DataFrame | Table,
+                        },
+                        ...
+                        ...
+                    ]
+
+"""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from histdatacom import Options, config
 from histdatacom.api import Api
+from histdatacom.cli import ArgParser
+from histdatacom.concurrency import QueueManager
+from histdatacom.csvs import Csv
 from histdatacom.influx import Influx
-
 from histdatacom.scraper.repo import Repo
 from histdatacom.scraper.scraper import Scraper
-
 from histdatacom.utils import Utils
 
-import sys
+if TYPE_CHECKING:
+    from datatable import Frame  # noqa:I900
+    from pandas import DataFrame
+    from pyarrow import Table
 
 
 class _HistDataCom:
-    """A module to pull market data from histdata.com and import it into influxDB"""
+    """Pull market data from histdata.com and import it into influxDB."""
 
     def __init__(self, options: Options) -> None:
+        """Initialize _HistDataCom Class.
 
-        """Initialization for _HistDataCom Class"""
-        # Set User () or Default Arguments respectively utilizing the self.ArgParser
-        # and self.Options classes.
-        #   - ArgParser()():
-        #       - ()(): use an IIFE to allow argparse to get garbage collected
-        #       - ()(): ArgParser.__call__ returns updated Options object
-        #       - vars(...): get the __dict__ representation of the object
-        #       - ArgParser._arg_list_to_set(...)
-        #           - Normalize iterable user arguments whose values are lists and
-        #             make them sets instead
-        #       - .copy(): decouple for GC using a hard copy of user args
-        config.ARGS = ArgParser.arg_list_to_set(
-            vars(ArgParser(options)())
+        Args:
+            options (Options): from histdata.options import Options
+
+        Set User () or Default Arguments respectively utilizing the
+        self.ArgParser and self.Options classes.
+          - ArgParser()():
+              - ()(): use an IIFE to allow argparse to get garbage collected
+              - ()(): ArgParser.__call__ returns updated Options object
+              - vars(...): get the __dict__ representation of the object
+              - ArgParser._arg_list_to_set(...)
+                  - Normalize iterable user arguments whose values are lists and
+                    make them sets instead
+              - .copy(): decouple for GC using a hard copy of user args
+        """
+        config.ARGS = ArgParser.arg_list_to_set(  # noqa:BLK100
+            vars(ArgParser(options)())  # noqa:WPS110
         ).copy()
         config.ARGS["default_download_dir"] = Utils.set_working_data_dir(
             config.ARGS["data_directory"]
@@ -53,8 +79,8 @@ class _HistDataCom:
         self.csvs = Csv()
         self.api = Api()
 
-        if (
-            config.ARGS["available_remote_data"]
+        if (  # noqa:BLK100
+            config.ARGS["available_remote_data"]  # noqa:BLK100
             or config.ARGS["update_remote_data"]
         ):
             if self.repo.test_for_repo_data_file():
@@ -64,12 +90,35 @@ class _HistDataCom:
         if config.ARGS["import_to_influxdb"] == 1:
             self.influx = Influx()
 
-    def run(self) -> list | dict | Frame | DataFrame | Table | None:
-        if (
-            config.ARGS["available_remote_data"]
+    def run(  # noqa:CCR001
+        self,
+    ) -> list | dict | Frame | DataFrame | Table | None:  # noqa:CCR001
+        """Execute. histdatacom's execution order.
+
+        Returns:
+            list | dict | Frame | DataFrame | Table | None:
+
+            Data (Frame | DataFrame | Table):
+                    a datatable Frame, a pandas DataFrame, or a pyarrow Table
+            List of dicts:  [
+                                {
+                                    "timeframe": timeframe,
+                                    "pair": pair,
+                                    "records": [record, record, ...],
+                                    "data": Frame | DataFrame | Table,
+                                },
+                                ...
+                                ...
+                            ]
+
+
+        """
+        if (  # noqa:BLK100
+            config.ARGS["available_remote_data"]  # noqa:BLK100
             or config.ARGS["update_remote_data"]
         ):
             return self.repo.get_available_repo_data()
+        del self.repo  # noqa:WPS100
 
         self.scraper.populate_initial_queue()
 
@@ -78,12 +127,15 @@ class _HistDataCom:
 
         if config.ARGS["download_data_archives"]:
             self.scraper.download_zips()
+            del self.scraper  # noqa:WPS100
             if config.ARGS["from_api"]:
                 self.api.validate_jays()
                 return self.api.merge_jays()
+        del self.api  # noqa:WPS100
 
         if config.ARGS["extract_csvs"]:
             self.csvs.extract_csvs()
+        del self.csvs  # noqa:WPS100
 
         if config.ARGS["import_to_influxdb"]:
             self.influx.import_data()
@@ -94,13 +146,33 @@ class _HistDataCom:
 def main(
     options: Options | None = None,
 ) -> list | dict | Frame | DataFrame | Table:
+    """Execute. Entry-point for histdatacom.
+
+    Args:
+        options (Options): a histdatacom.options Options object.
+
+    Returns:
+        list | dict | Frame | DataFrame | Table:
+
+            Data (Frame | DataFrame | Table):
+                    a datatable Frame, a pandas DataFrame, or a pyarrow Table
+            List of dicts:  [
+                                {
+                                    "timeframe": timeframe,
+                                    "pair": pair,
+                                    "records": [record, record, ...],
+                                    "data": Frame | DataFrame | Table,
+                                },
+                                ...
+                                ...
+                            ]
+    """
     if not options:
         options = Options()
         QueueManager(options)(_HistDataCom)
         return None
-    else:
-        options.from_api = True
-        return QueueManager(options)(_HistDataCom)
+    options.from_api = True
+    return QueueManager(options)(_HistDataCom)
 
 
 if __name__ == "__main__":
